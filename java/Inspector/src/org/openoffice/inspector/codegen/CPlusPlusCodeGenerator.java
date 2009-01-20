5f182d37037a1d4d849be5302c8a4e667b73651d
cli <cli@8da58560-a4e7-4996-a0c2-a735b94b261c>
2009-01-20 17:48:45 +0000

    git-svn-id: http://svn.services.openoffice.org/ooo/contrib/sdk/examples@266619 8da58560-a4e7-4996-a0c2-a735b94b261c

--- java/Inspector/src/org/openoffice/inspector/codegen/CPlusPlusCodeGenerator.java
+++ java/Inspector/src/org/openoffice/inspector/codegen/CPlusPlusCodeGenerator.java
@@ -35,12 +35,20 @@
 package org.openoffice.inspector.codegen;
 
 import com.sun.star.reflection.XIdlMethod;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.openoffice.inspector.util.Resource;
 import org.openoffice.inspector.util.StringTemplate;
 
 public class CPlusPlusCodeGenerator 
   extends CodeGenerator
 {
+  
+  private Set<String> properties      = new HashSet<String>();
+  private Set<String> queryInterfaces = new HashSet<String>();
+  private List<XIdlMethod> invokeMethods = new ArrayList<XIdlMethod>();
   
   private StringTemplate tmplProgram = new StringTemplate(
     Resource.getAsString("org/openoffice/inspector/codegen/template/CPlusPlusProgramStub.tmpl"));
@@ -62,19 +70,38 @@ public class CPlusPlusCodeGenerator
   @Override
   public void addAccessorCodeFor(String property)
   {
-
+    if(!this.properties.contains(property))
+    {
+      this.properties.add(property);
+      fireCodeUpdateEvent();
+    }
   }
 
   @Override
   public void addInvokeCodeFor(XIdlMethod method)
   {
-
+    if(!this.invokeMethods.contains(method))
+    {
+      this.invokeMethods.add(method);
+      fireCodeUpdateEvent();
+    }
   }
   
   @Override
   public void addQueryCodeFor(String iface)
   {
-    
+    if(this.queryInterfaces.contains(iface))
+    {
+      this.queryInterfaces.add(iface);
+      fireCodeUpdateEvent();
+    }
+  }
+  
+  protected void fireCodeUpdateEvent()
+  {
+    CodeUpdateEvent event = new CodeUpdateEvent(
+      getSourceCode(), Language.Java, 0);
+    super.fireCodeUpdateEvent(event);
   }
   
 }
